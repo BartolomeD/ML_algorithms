84f899e453bcc8072fb700fedc75ff8709595810
BartolomeD <d.bartolome.r@gmail.com>
2017-08-14 14:48:15 +0200

    added numpy compatibility

--- stack_ensembler/stack_ensembler.py
+++ stack_ensembler/stack_ensembler.py
@@ -1,35 +1,35 @@
 import numpy as np
 import pandas as pd
+from collections import OrderedDict
 from sklearn.model_selection import KFold
 
 ''' Function to perform stack ensembling on dataset with sklearn models.
 
 # Arguments:
     models:  list, a list of models to be used to create meta-features
-    X:       dataframe, the trainset features
-    y:       dataframe, the trainset labels
-    Xtest:   dataframe, the testset features
+    x_train: ndarray, the trainset features
+    y_train: array, the trainset labels
+    x_test:  ndarray, the testset features
     splits:  int, the number of splits for trainset CV
     verbose: bool, true when print outputs are desired
     
 # Returns:
-    X:       dataframe, new trainset with meta-features
-    Xtest:   dataframe, new testset with meta-features
+    x_train: ndarray, new trainset with meta-features
+    x_test:  ndarray, new testset with meta-features
 '''
 
 def stack_ensemble(models, x_train, y_train, x_test, splits=5, verbose=True):
         
     # join data and meta features
     def _join(x, preds):
-        meta = pd.DataFrame(columns = [col for col in preds.keys()])
-        for model, y_hat in preds.items():
-            meta[model] = pd.Series(y_hat)
-        return pd.concat([x, meta], axis=1)
+        for pred in preds.values():
+            x = np.concatenate([x, np.array(pred).reshape(-1, 1)], axis=1)
+        return x
     
     # init variables
     kf = KFold(n_splits = splits)
-    preds_train = {}
-    preds_test = {}
+    preds_train = OrderedDict()
+    preds_test = OrderedDict()
 
     # iterate over all inserted models
     for name, model in [(str(type(model)).split('.')[-1][:-2], model) for model in models]:
@@ -38,10 +38,9 @@ def stack_ensemble(models, x_train, y_train, x_test, splits=5, verbose=True):
 
         # train and predict
         for train, test in kf.split(x_train):
-            model.fit(x_train.iloc[train], y_train[train])
-            preds_train[name].extend(model.predict(x_train.iloc[test]))
+            model.fit(x_train[train], y_train[train])
+            preds_train[name].extend(model.predict(x_train[test]))
         preds_test[name] = model.predict(x_test)
-        
+
     # return trainset and testset with meta features
     return _join(x_train, preds_train), _join(x_test, preds_test)
-    
